4ce85d10d86e1c61c5b6fb6742f3b206bedd6eb2
DiskyNorsk <110564749+DiskyNorsk@users.noreply.github.com>
2022-08-03 21:11:26 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,263 @@
+@namespace
+class SpriteKind:
+    UI_element = SpriteKind.create()
+StartupAnimation = sprites.create(img("""
+        f f f f f f f f f f f f f f f f f 
+            f f f f f f f f f f f f f f f f f 
+            f f f f f f f f f f f f f f f f f 
+            f f f f f f f f f f f f f f f f f 
+            f f f f 1 f f f f f f f 1 f f f f 
+            f f f f f 1 f f f f f 1 f f f f f 
+            f f f f f f 1 f f f 1 f f f f f f 
+            f f f f f f f 1 f 1 f f f f f f f 
+            f f f f f f f f 1 f f f f f f f f 
+            f f f f f f f 1 f 1 f f f f f f f 
+            f f f f f f 1 f f f 1 f f f f f f 
+            f f f f f 1 f f f f f 1 f f f f f 
+            f f f f 1 f f f f f f f 1 f f f f 
+            f f f f f f f f f f f f f f f f f 
+            f f f f f f f f f f f f f f f f f 
+            f f f f f f f f f f f f f f f f f 
+            f f f f f f f f f f f f f f f f f
+    """),
+    SpriteKind.UI_element)
+pause(3000)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f f f f f f 1 f f f f f 
+        f f f f f 1 f f f f 1 f f f f f f 
+        f f f f f f 1 f f 1 f f f f f f f 
+        f f f f f f f 1 1 f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f f 1 1 f f f f f f f f 
+        f f f f f f 1 f f 1 f f f f f f f 
+        f f f f f 1 f f f f 1 f f f f f f 
+        f f f f 1 f f f f f f 1 f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f f f f f 1 f f f f f f 
+        f f f f f 1 f f f 1 f f f f f f f 
+        f f f f f 1 f f 1 f f f f f f f f 
+        f f f f f f 1 f 1 f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f 1 f 1 f f f f f f f f 
+        f f f f f 1 f f 1 f f f f f f f f 
+        f f f f f 1 f f f 1 f f f f f f f 
+        f f f f 1 f f f f f 1 f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f f f f 1 f f f f f f f 
+        f f f f 1 f f f 1 f f f f f f f f 
+        f f f f f 1 f f 1 f f f f f f f f 
+        f f f f f f 1 1 f f f f f f f f f 
+        f f f f f f 1 f f f f f f f f f f 
+        f f f f f f 1 1 f f f f f f f f f 
+        f f f f f 1 f f 1 f f f f f f f f 
+        f f f f 1 f f f 1 f f f f f f f f 
+        f f f f 1 f f f f 1 f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f f f 1 f f f f f f f f 
+        f f f f 1 f f 1 f f f f f f f f f 
+        f f f f f 1 f 1 f f f f f f f f f 
+        f f f f f 1 1 f f f f f f f f f f 
+        f f f f f f 1 f f f f f f f f f f 
+        f f f f f f 1 f f f f f f f f f f 
+        f f f f f 1 f 1 f f f f f f f f f 
+        f f f f f 1 f 1 f f f f f f f f f 
+        f f f f 1 f f f 1 f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f 1 f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f f 1 f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f 1 f 1 f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 1 f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f 1 f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f 1 f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f 1 f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f 1 f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f 1 f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f 1 1 f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f 1 f f f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f 1 f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f f
+"""))
+pause(50)
+StartupAnimation.set_image(img("""
+    . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . .
+"""))
